0024682b7cc52cedc5ea727026f8d5e7fa4d48ab
sungdongyoon <ehdtjd0906@naver.com>
2023-06-08 00:59:47 +0900

    06/08 01:00

--- 우산있어 팀 프로젝트/js/2-1.member_buy.js
+++ 우산있어 팀 프로젝트/js/2-1.member_buy.js
@@ -13,37 +13,46 @@ const ticketPrice = document.querySelector(".buy_modal_price_right");
 ticketOne.addEventListener("click", () => {
   paymentModal.classList.add("active");
   /* 세부사항 */
-  ticketType.innerText = "1회권";
   ticketPrice.innerText = "8,000원(보증금 7,000원 + 대여료 1,000원)";
 })
 ticketThree.addEventListener("click", () => {
   paymentModal.classList.add("active");
   /* 세부사항 */
-  ticketType.innerText = "3회권";
   ticketPrice.innerText = "20,700원(보증금 18,000원 + 대여료 2,700원)";
 })
 ticketFive.addEventListener("click", () => {
   paymentModal.classList.add("active");
   /* 세부사항 */
-  ticketType.innerText = "5회권";
   ticketPrice.innerText = "29,000원(보증금 25,000원 + 대여료 4,000원)";
 })
 
+/* 이용권 타입 */
 const ticket = document.querySelectorAll(".member_ticket_buy");
+const ticketArr = [1, 3, 5];
 
-let ticketValue = 1;
 for(let i = 0; i < ticket.length; i++) {
-  ticketValue += i;
   ticket[i].addEventListener("click", () => {
-    ticketType.innerText = `${i}회권`;
+    ticketType.innerText = `${ticketArr[i]}회권`;
   })
 }
-console.log(ticketValue);
 
+/* 약관동의 */
+const agreeAll = document.querySelector(".buy_modal_agreeAll");
+const agrees = document.querySelectorAll(".buy_modal_agree input");
 
+const checkAll = (target) => {
+  const isChecked = target.checked;
+
+  agrees.forEach((el) => {
+    el.checked = isChecked;
+  })
+}
+
+/* 모달 취소 */
 cancelBtn.addEventListener("click", () => {
   paymentModal.classList.remove("active");
 })
+/* 모달 결제하기 */
 buyBtn.addEventListener("click", () => {
   location.href = "/html/4.buy_complete.html";
 })
--- 우산있어 팀 프로젝트/js/4.buy_complete.js
+++ 우산있어 팀 프로젝트/js/4.buy_complete.js
@@ -1,6 +1,23 @@
+/* 페이지 이동 */
 const goHome = document.querySelector(".go_home");
 const goRent = document.querySelector(".go_rent");
 
 goHome.addEventListener("click", () => {
   location.href = "/1.member.html";
 })
+
+goRent.addEventListener("click", () => {
+  location.href = "/html/5.rent.html";
+})
+
+/* 세부정보 */
+const paymentTicket = document.querySelector(".payment_ticket");
+const paymentTime = document.querySelector(".payment_time");
+const paymentPrice = document.querySelector(".payment_price");
+const paymentMethod = document.querySelector(".payment_method");
+
+paymentTicket.innerText = "1회";
+
+const date = new Date();
+paymentTime.innerHTML = `${date.getFullYear()}.${String(date.getMonth() + 1).padStart(2, "0")}.${String(date.getDate()).padStart(2, "0")} ${String(date.getHours()).padStart(2, "0")}:${String(date.getMinutes()).padStart(2, "0")}`;
+
